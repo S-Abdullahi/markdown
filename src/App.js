8893f5b6ea787e30312ed77587995a7c6ef04cfe
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-23 15:18:29 +0100

    markdown commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("## Markdown");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 className="text-center mt-2 text-2xl font-bold">Markdown Project</h2>
+      <div className="bg-slate-300 h-screen grid grid-cols-2 gap-6 m-10 p-4">
+        <form className="bg-slate-300 w-full h-full">
+          <textarea
+            value={text}
+            className="w-full p-4 h-full bg-white rounded"
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </form>
+        <div className="bg-slate-600 p-4 rounded text-white">
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
